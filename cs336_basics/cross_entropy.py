b8bc4fa1adf38fe39dbb2c6124bf5aa0b43f83e7
George Chen <qchea@amazon.com>
2025-08-08 18:27:49 -0500

    MNT: redundant operation

--- cs336_basics/cross_entropy.py
+++ cs336_basics/cross_entropy.py
@@ -6,7 +6,7 @@ def cross_entropy(
         logits: Float[Tensor, " batch_size vocab_size"], targets: Int[Tensor, " batch_size"]) -> Float[Tensor, ""]:
     logits_max = torch.amax(logits, dim=-1, keepdim=True)
     logits_centered = logits - logits_max
-    logits_exp = torch.exp(logits - logits_max)
+    logits_exp = torch.exp(logits_centered)
     logits_exp_sum = logits_exp.sum(dim=-1)
     log_logits_exp_sum = torch.log(logits_exp_sum)
     summation = - logits_centered[torch.arange(logits.shape[0]), targets] + log_logits_exp_sum
