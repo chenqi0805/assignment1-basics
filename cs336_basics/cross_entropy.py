6680d1c9c5a98ff828d6fd227abe2829fccdea03
George Chen <qchea@amazon.com>
2025-08-08 19:06:59 -0500

    ENH: cross_entropy implementation

--- cs336_basics/cross_entropy.py
+++ cs336_basics/cross_entropy.py
@@ -4,10 +4,7 @@ import torch
 
 def cross_entropy(
         logits: Float[Tensor, " batch_size vocab_size"], targets: Int[Tensor, " batch_size"]) -> Float[Tensor, ""]:
-    logits_max = torch.amax(logits, dim=-1, keepdim=True)
-    logits_centered = logits - logits_max
-    logits_exp = torch.exp(logits_centered)
-    logits_exp_sum = logits_exp.sum(dim=-1)
-    log_logits_exp_sum = torch.log(logits_exp_sum)
-    summation = - logits_centered[torch.arange(logits.shape[0]), targets] + log_logits_exp_sum
-    return summation.mean()
+    logsumexp = torch.logsumexp(logits, dim=-1)
+    target_logits = torch.gather(logits, 1, targets[:, None]).squeeze(1)
+    loss = logsumexp - target_logits
+    return loss.mean()
